738e3466871091364359334d9bf4b2cad22621c2
nms377 <nms377@nyu.edu>
2017-03-18 20:13:47 -1000

    ReactDOM wasn't accepting my second element but I Googled and found a solution on StackOverflow and I placed it in a div because the function only expected one element not two.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import Form from './containers/Form';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+	<div>
+	  <App />
+	  <Form />
+	</div>,  
   document.getElementById('root')
 );
