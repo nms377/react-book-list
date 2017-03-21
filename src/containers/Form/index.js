c01c2d245ee70bcf947778789e5f08c0581ec0cf
nms377 <nms377@nyu.edu>
2017-03-20 22:40:04 -1000

    On Saturday, I got my form to render in the browser, but it populated a bunch of [object] arrays and wouldn't allow me to type in the form. Tonight, I reviewed the docs on the form to see what I could do to get the form to allow me to type stuff. I console logged my value key in the handleChange function and for at least an hour and a half it showed the text was undefined. I realized I was getting too specific calling the keys inside the value object when I should have left it as value. I used the specific keys inside my input tags, but I get a warning that I'm changing my component from an uncontrolled to a controlled component.

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
-import BookList from '../../data/BookList.js';
+// import BookList from '../../data/BookList.js';
 
 class AddBook extends Component {
 	constructor(){
 		super();
 		this.state = {
-			book: BookList
+			value: {
+				title: '',
+				author: ''
+			}
 		}
 		this.handleChange = this.handleChange.bind(this);
+
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(event) {
-		this.setState({value: event.target.book});
+		console.log(event.target.value);
+		this.setState({value: {
+			title: event.target.value.title, author: event.target.value.author
+		}});
 	}
 
 	handleSubmit(event) {
-		alert('A book was submitted: ' + this.state.book);
+		alert('A book was submitted: ' + this.state.value.title + ' by ' +this.state.value.author);
 		event.preventDefault();
-	}
+	};
 
-	render(){
+	render(AddBook){
 		return (
       <form onSubmit={this.handleSubmit}>
         <label>
           Title:
-          <input type="text" value={this.state.book} onChange={this.handleChange}/>
+          <input type="text" value={this.state.value.title} onChange={this.handleChange} placeholder="Title of Book"/>
         </label>
         <label>
           Author:
-          <input type="text" value={this.state.book} onChange={this.handleChange}/>
+          <input type="text" value={this.state.value.author} onChange={this.handleChange}  placeholder="Who Wrote the Book"/>
         </label>
-        <button type="submit">Add Book</button>
+        <button type="submit" value="Submit">Add Book</button>
       </form>
 		);
 	}
