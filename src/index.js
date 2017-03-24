0bcbf95a044ada5f87279955ce736c6b8cf5b4cc
nms377 <nms377@nyu.edu>
2017-03-23 20:17:49 -1000

    Moved connect and store into the src/index.js file. I need to go back and refactor, I had a lot of erros in my App/index.js file. The main one being my getBooksFromFakeXHR not being defined.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import App from './containers/App';
 import Form from './containers/Form';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import books from './reducers';
+
+let store = createStore(books);
+
 ReactDOM.render(
 	<div>
-	  <App />
-	  <Form />
+		<Provider store={store}>
+		  <App />
+		  <Form />
+	  </Provider>
 	</div>,  
   document.getElementById('root')
 );
