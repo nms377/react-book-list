cb2f0c961da94a48a3c75a256ae96315d74b5eff
nms377 <nms377@nyu.edu>
2017-03-29 19:45:20 -1000

    Commented out stuff cause it was breaking.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,71 +1,101 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import BookListAppTitle from '../../components/BookListAppTitle.js';
 import BookList from '../../data/BookList.js';
 import AddBook from '../Form';
 import './styles.css';
 
-import { createStore } from 'redux';
-
-// import reducer
 import books from '../../reducers';
 
 // import actions
-import { addBook } from '../../actions';
+import { addBook, updateBooks } from '../../actions';
 
+
+// let store = createStore(books);
 // create store
 // pass reducer to store
-let store = createStore(books);
 
-store.dispatch(addBook('HACK THE PLANET', 'BORGZ'));
+// store.dispatch(addBook('HACK THE PLANET', 'BORGZ'));
 
-console.log(store.getState(books));
+// console.log(store.getState(books));
 
 class App extends Component {
   constructor(){
     super();
-    this.state ={
-      book: []
-    }
+
+    //  no longer need to set the state if you use redux
+    //  state is handled by redux and the store
+    //  you do need to have this for your form though
+    //  can put in your form component
+    // this.state ={
+    //   book: []
+    // }
+
   }
 
-  XHRRequest() {
-    return new Promise((resolve, reject) => {
-      resolve(BookList)
-    })
-  }
+  // addBook = (book) => {
+    // addBookToFakeXHR(book)
+    //   .then(book => {
+      //  no more setting states in redux
+    //     this.setState({books});
+    //   });
+    //  trigger action
+  //   this.props.addBook(book);
+  // }
 
-  addBook = (book) => {
-    this.setState({
-      books: this.state.books.concat([book])
-    })
-  }
+  // componentWillMount() {
+   // getBooksFromFakeXHR()
+   //  .then( books => {
+   //    books.forEach( book => {
+   //      //  internal state way
+   //      // store.dispatch(addBook(book.title, book.author))
+   //      this.props.updateBooks(books)
+   //    });
+   //  });
+  // }
 
-  componentWillMount() {
-    this.XHRRequest().then(book => {
-      this.setState({
-        book
-      })
-    })
-  }
 
   render() {
     return (
       <div className="App">
-        <ul>
-          {
-            this.state.book.map( ( { title, author } ) =>
-              <BookListAppTitle
-                key={title}
-                title={title}
-                author={author}
-              />
-            )
-          }
-        </ul>
+        <h2>React Book List</h2>
+        <div>
+          <ul>
+            {
+              this.props.books.map( ( { title, author } ) =>
+                <BookListAppTitle
+                  key={title}
+                  title={title}
+                  author={author}
+                />
+              )
+            }
+          </ul>
         <AddBook addBook={this.addBook}/>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    books: state.books
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddBook: (title, author) => {
+      dispatch(addBook(title, author));
+    },
+    updateBooks: books => {
+      dispatch(updateBooks(books));
+    }
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
