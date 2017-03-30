d8e53d2faad406f044337c5989d92fadbee88ea2
nms377 <nms377@nyu.edu>
2017-03-29 19:46:59 -1000

    Commented out stuff cause it was breaking.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,36 @@
-import { ADD_BOOK } from '../actions';
+// import { ADD_BOOK } from '../actions';
 
-const initialState = {
-	books: []
-};
+// const initialState = {
+// 	books: []
+// };
 
-// es2015 feature allows us to set default value for parameter
-function books(state = initialState, action) {
-  switch(action.type) {
-    case ADD_BOOK:
-      return Object.assign({}, state, {
-        books: [
-        	//	new feature ... is a spread operator
-        	//	
-          ...state.books,
-          {
-            title: action.title,
-            author: action.author
-          }
-        ]
-      })
-    default:
-      return state;
-  }
-}
+// // es2015 feature allows us to set default value for parameter
+// function books(state = initialState, action) {
+//   switch(action.type) {
+//     case ADD_BOOK:
+//       return Object.assign({}, state, {
+//         books: [
+//         	//	new feature ... is a spread operator
+//         	//	
+//           ...state.books,
+//           {
+//             title: action.title,
+//             author: action.author
+//           }
+//         ]
+//       })
+//     default:
+//       return state;
+//   }
+// }
 
-export default books;
+// export default books;
+
+import { combineReducers } from 'redux'
+import books from './books';
+
+const reducers = combineReducers({
+  books
+});
+
+export default reducers;
