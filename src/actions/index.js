144b06cc310428930590263da0044d2ae2c81800
nms377 <nms377@nyu.edu>
2017-03-29 19:44:25 -1000

    Commented out stuff cause it was breaking.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,45 @@
+// when you create an action immediately add it to your reducers
+
+import { addBookToFakeXHR } from '../data/BookList.js';
+
 export const ADD_BOOK = 'ADD_BOOK';
 
-export function addBook(title, author) {
-	return {
-		type: ADD_BOOK,
-		//	es2015 lets you write shorthand instead of title: this.title
-		title,
-		author
+// export function addBook(title, author) {
+// 	return {
+// 		type: ADD_BOOK,
+// 		//	es2015 lets you write shorthand instead of title: this.title
+// 		title,
+// 		author
+// 	};
+// };
+
+export const addBook = books => {
+	return function (dispatch) {
+		//asynce actions
+		//thunk
+
+		dispatch({
+			type: 'ADDING_BOOK',
+			// book
+		})
+		
+		// addBookFromFakeXHR(books)
+		// 	.then(books => {
+		// 		dispatch({
+		// 			type:'ADD_BOOK',
+		// 			books
+		// 		});
+		// 	});
 	}
 }
 
+
+export const updateBooks = books => {
+	// must return a valid action
+
+	return {
+		type: 'UPDATE_BOOKS',
+		// all other args to pass to my reducer function
+		books
+	};
+};
