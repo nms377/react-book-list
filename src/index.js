3cb9b81ab820d9c14d5ba0f4784f73947bc8132c
nms377 <nms377@nyu.edu>
2017-03-29 19:45:47 -1000

    Imported react-router-dom and imported my dummy components and Nav components.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
+//external dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
-import Form from './containers/Form';
-import './index.css';
-
+import { 
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import books from './reducers';
+import ReduxThunk from 'redux-thunk';
+import { createDevTools } from 'redux-devtools';
 
-let store = createStore(books);
+// internal dependencies
+import App from './containers/App';
+import Form from './containers/Form';
+import About from './components/About';
+import FavoriteBook from './components/FavoriteBook';
+import Nav from './components/Nav';
+import './index.css';
+import reducers from './reducers';
+
+//	reducers 
+let store = createStore(reducers, 
+	//	for use in development only
+	//	don't keep when you go to production 
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+	// applyMiddleware(ReduxThunk)
+);
 
 ReactDOM.render(
 	<div>
 		<Provider store={store}>
-		  <App />
-		  <Form />
+		  <Router>
+		  	<div>
+		  		<div>
+		  			<Nav />
+		  		</div>
+		  		<Route exact path="/" component={App} />
+		  		<Route path="/about" component={About}/>
+		  		<Route path="/favebook" component={FavoriteBook}/>
+		  	</div>
+		  </Router>	
 	  </Provider>
 	</div>,  
   document.getElementById('root')
